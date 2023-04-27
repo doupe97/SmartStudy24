4409ea6535553423fb43696ef278aebd553abd0e
Nico Müller <85606446+doupe97@users.noreply.github.com>
2023-04-27 12:27:24 +0200

    Added amplify code

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,26 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "SmartStudy": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    },
+    "api": {
+        "SmartStudy": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    },
+    "storage": {
+        "s3smartstudystorageee0e1ba4": {
+            "BucketName": "string",
+            "Region": "string"
+        }
+    }
+}
